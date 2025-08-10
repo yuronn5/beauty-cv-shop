fc2275a6c9fd489c91e46429cfee06480bea9171
yurii bilous <yurii.b@elogic.com.ua>
2025-08-10 00:22:47 -0500

    bugf

--- js/main.js
+++ js/main.js
@@ -233,7 +233,7 @@ preloader
 
 
         $.ajax({
-            url: "/netlify/functions/contact",
+            url: "/netlify/functions/contact.js",
             method: "POST",
             contentType: "application/json",
             data: JSON.stringify(payload),
